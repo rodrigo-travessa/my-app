59d78888c056d73ad854c02904e3f539806102f6
RodrigoTravessa <rodrigosigiliao@outlook.com>
2025-08-21 14:03:04 -0300

    simple calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
